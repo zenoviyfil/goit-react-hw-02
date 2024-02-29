f830159eceea3ae8f7d3222d6e3859d469b381ef
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-02-29 16:32:48 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,54 +13,54 @@ const feedback = {
 }
 
 function App() {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-    const [total, setTotal] = useState(0)
-    const [positive, setPositive] = useState(0)
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
 
-    const updateGood = () => {
-      setGood(good + 1)
-    }
+    // const updateGood = () => {
+    //   setGood(good + 1)
+    // }
  
-    const updateNeutral = () => {
-      setNeutral(neutral + 1)
-    }
+    // const updateNeutral = () => {
+    //   setNeutral(neutral + 1)
+    // }
  
-    const updateBad = () => {
-      setBad(bad + 1)
-    }
+    // const updateBad = () => {
+    //   setBad(bad + 1)
+    // }
 
-    const updateTotal = () => {
-      setTotal(good + neutral + bad)
-    }
+    // const updateTotal = () => {
+    //   setTotal(good + neutral + bad)
+    // }
 
-    const onReset = () => {
-      setGood(0)
-      setNeutral(0)
-      setBad(0)
-      setTotal(0)
-      setPositive(0)
-    }
+    // const onReset = () => {
+    //   setGood(0)
+    //   setNeutral(0)
+    //   setBad(0)
+    //   setTotal(0)
+    //   setPositive(0)
+    // }
 
     useEffect(() => {
       localStorage.setItem("feedback", JSON.stringify(feedback));
-    }, [])
+    }, [feedback])
 
-    useEffect(() => {
-      setTotal(good + neutral + bad)
-    }, [good, neutral, bad])
+    // useEffect(() => {
+    //   setTotal(good + neutral + bad)
+    // }, [good, neutral, bad])
  
-    useEffect(() => {
-      setPositive(Math.round(((good + neutral) / total) * 100))
-    }, [good, neutral, total])
+    // useEffect(() => {
+    //   setPositive(Math.round(((good + neutral) / total) * 100))
+    // }, [good, neutral, total])
 
   return (
     <>
       <Description />
       <Options updateGood={updateGood} updateNeutral={updateNeutral} updateBad={updateBad} updateTotal={updateTotal} total={total} onReset={onReset} feedback={feedback}/>
       {total ? 
-      (<Feedback good={good} neutral={neutral} bad={bad} total={total} positive={positive} feedback={feedback}/>) : <Notification />}
+      (<Feedback good={feedback.good} neutral={feedback.neutral} bad={feedback.bad} total={total} positive={positive} feedback={feedback}/>) : <Notification />}
     </>
   )
 }
